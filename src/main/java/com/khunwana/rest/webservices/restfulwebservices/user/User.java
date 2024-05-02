a176150f8cf643d45ec7daa5db9ebbef754a82d2
Monias <khunwana.monias@gmail.com>
2024-05-03 00:49:09 +0200

    Creation of methods to find and show GET methods and POST method
    Signed-off-by: Monias <khunwana.monias@gmail.com>

---
+++ src/main/java/com/khunwana/rest/webservices/restfulwebservices/user/User.java
@@ -0,0 +1,52 @@
+package com.khunwana.rest.webservices.restfulwebservices.user;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+//@Entity(name="user_details")
+public class User {
+	
+	public User(int id, String name, LocalDate birthDate) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.birthDate = birthDate;
+	}
+	
+//	@Id
+//	@GeneratedValue
+	private int id;
+	
+	private String name;
+	private LocalDate birthDate;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", birthDate=" + birthDate + "]";
+	}
+	
+	
+	
+	
+	
+}
---
+++ src/main/java/com/khunwana/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -0,0 +1,45 @@
+package com.khunwana.rest.webservices.restfulwebservices.user;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDaoService {
+	
+	public static List<User> users = new ArrayList<>();
+	private static int usersCount = 0;
+	
+	static {
+		users.add(new User(++usersCount,"Monias",LocalDate.now().minusYears(30)));
+		users.add(new User(++usersCount,"Nqobizitha",LocalDate.now().minusYears(24)));
+		users.add(new User(++usersCount,"Khunwana",LocalDate.now().minusYears(20)));
+	}
+	
+	public List<User> findAll()
+	{
+		return users;
+	}
+	
+//	public void save(User user)
+//	{
+//		users.add(user);
+//	}
+//	
+	public User findOne(int id)
+	{
+		Predicate<? super User> predicate = user -> user.getId() == id;
+		return users.stream().filter(predicate).findFirst().get();
+//		return users.get(id-1);
+	}
+
+	public User save(User user)
+	{
+		user.setId(++usersCount);
+		users.add(user);
+		return user;
+	}
+}

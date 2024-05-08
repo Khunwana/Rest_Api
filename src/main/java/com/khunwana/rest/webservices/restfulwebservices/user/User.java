7c4c1fafb80845aacf0eab1d658f8cb340072d42
Monias <khunwana.monias@gmail.com>
2024-05-08 16:09:09 +0200

    validation annotation implementation
    Signed-off-by: Monias <khunwana.monias@gmail.com>

--- src/main/java/com/khunwana/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/khunwana/rest/webservices/restfulwebservices/user/User.java
@@ -5,6 +5,8 @@ import java.time.LocalDate;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 
 //@Entity(name="user_details")
 public class User {
@@ -20,7 +22,10 @@ public class User {
 //	@GeneratedValue
 	private int id;
 	
+	@Size(min=2, message = "Name should have atleast 2 characters") //annotation from validation to check if a minimum of 2 character has been entered
 	private String name;
+	
+	@Past(message = "Birth date should always be in the past") //annotation to check if the date entered is in the past of current date
 	private LocalDate birthDate;
 	public int getId() {
 		return id;

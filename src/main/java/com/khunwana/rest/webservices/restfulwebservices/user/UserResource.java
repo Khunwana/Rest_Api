1e4d2935f18836b45f14a9ce64eb36a484e52fdd
Monias <khunwana.monias@gmail.com>
2024-05-09 18:32:03 +0200

    Hateoas method modification for link to all users
    Signed-off-by: Monias <khunwana.monias@gmail.com>

--- src/main/java/com/khunwana/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/khunwana/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,8 +1,11 @@
 package com.khunwana.rest.webservices.restfulwebservices.user;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 import java.net.URI;
 import java.util.List;
 
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,7 +34,7 @@ public class UserResource {
 	}
 	
 	@GetMapping("/users/{id}")
-	public User retrieveUser(@PathVariable int id)
+	public EntityModel<User> retrieveUser(@PathVariable int id)
 	{
 		User user = service.findOne(id);
 		
@@ -39,7 +42,12 @@ public class UserResource {
 		{
 			throw new UserNotFoundException("Id :"+id);
 		}
-		return user;
+		
+		EntityModel<User> entityModel = EntityModel.of(user);
+		
+		WebMvcLinkBuilder linkBuilder = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+		entityModel.add(linkBuilder.withRel("all-users"));
+		return entityModel;
 	}
 	
 	@PostMapping("/users")

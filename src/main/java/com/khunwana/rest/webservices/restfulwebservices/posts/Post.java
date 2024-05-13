4375676f3e23eb79847ddf8ea01c58c5054006f1
Monias <khunwana.monias@gmail.com>
2024-05-13 23:09:51 +0200

    update default constructor
    Signed-off-by: Monias <khunwana.monias@gmail.com>

--- src/main/java/com/khunwana/rest/webservices/restfulwebservices/posts/Post.java
+++ src/main/java/com/khunwana/rest/webservices/restfulwebservices/posts/Post.java
@@ -1,39 +1,65 @@
 package com.khunwana.rest.webservices.restfulwebservices.posts;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.khunwana.rest.webservices.restfulwebservices.user.User;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Post {
 	
-	public Post(int id, String decription) {
+	public Post()
+	{
+		
+	}
+	
+	public Post(int id, String description) {
 		super();
 		this.id = id;
-		this.decription = decription;
+		this.description = description;
 	}
 	
 	@Id
 	@GeneratedValue
 	private int id;
-	private String decription;
+	private String description;
+	
+	@ManyToOne(fetch =  FetchType.LAZY)
+	@JsonIgnore
+	private User user;
 	
 	
+	
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
 	public int getId() {
 		return id;
 	}
 	public void setId(int id) {
 		this.id = id;
 	}
-	public String getDecription() {
-		return decription;
-	}
-	public void setDecription(String decription) {
-		this.decription = decription;
-	}
+
 	@Override
 	public String toString() {
-		return "Post [id=" + id + ", decription=" + decription + "]";
+		return "Post [id=" + id + ", decription=" + description + "]";
 	}
 	
 	

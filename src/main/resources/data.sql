93bcdb821a464596cc56fbb4a26b19c8dc122a12
Monias <khunwana.monias@gmail.com>
2024-05-13 23:13:36 +0200

    insertion queries for post
    Signed-off-by: Monias <khunwana.monias@gmail.com>

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,3 +4,13 @@ insert into user_details(birth_date,id,name)
 values(current_date(),10002,'Nqobizitha');
 insert into user_details(birth_date,id,name)
 values(current_date(),10003,'Khunwana');
+
+insert into post(id,user_id,description)
+values(20001,10001,'testing 1');
+insert into post(id,user_id,description)
+values(20002,10001,'testing 2');
+
+insert into post(id,user_id,description)
+values(20003,10002,'testing 3');
+insert into post(id,user_id,description)
+values(20004,10002,'testing 4');

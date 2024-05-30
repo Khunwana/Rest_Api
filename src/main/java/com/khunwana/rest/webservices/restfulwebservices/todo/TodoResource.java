90b830004774882413b0e2b5282e937c0918a49b
Monias <khunwana.monias@gmail.com>
2024-05-30 04:03:41 +0200

    To-Do creation and restController for To-Do
    Signed-off-by: Monias <khunwana.monias@gmail.com>

---
+++ src/main/java/com/khunwana/rest/webservices/restfulwebservices/todo/TodoResource.java
@@ -0,0 +1,53 @@
+package com.khunwana.rest.webservices.restfulwebservices.todo;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TodoResource {
+	
+	private TodoService to_doService;
+	
+	public TodoResource(TodoService to_doService)
+	{
+		this.to_doService = to_doService;
+	}
+	
+	@GetMapping("/users/{username}/todos")
+	public List<Todo> retrieveTo_Do(@PathVariable String username)
+	{
+		return to_doService.findByUsername(username);
+	}
+	
+	@GetMapping("/users/{username}/todos/{id}")
+	public Todo retrieveTo_Do(@PathVariable String username ,@PathVariable int id)
+	{
+		return to_doService.findById(id);
+	}
+	
+	@DeleteMapping("/users/{username}/todos/{id}")
+	public ResponseEntity<Void> deleteTo_DO(@PathVariable String username ,@PathVariable int id)
+	{
+		to_doService.deleteById(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping("/users/{username}/todos/{id}")
+	public void updateTo_DO(@PathVariable String username ,@PathVariable int id)
+	{
+		
+	}
+	
+	@PostMapping("/users/{username}/todos")
+	public void createTo_Do(@PathVariable String username)
+	{
+		
+	}
+}
---
+++ src/main/java/com/khunwana/rest/webservices/restfulwebservices/todo/TodoService.java
@@ -0,0 +1,53 @@
+package com.khunwana.rest.webservices.restfulwebservices.todo;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import org.springframework.stereotype.Service;
+import jakarta.validation.Valid;
+
+@Service
+public class TodoService {
+	private static ArrayList<Todo> todos= new ArrayList<Todo>();;
+	private static int todoCount = 0;
+
+	static {
+		todos.add(new Todo(++todoCount,"Khunwana", "master Springboot",LocalDate.now().plusMonths(1),false));
+		todos.add(new Todo(++todoCount,"Khunwana", "master SpringSecurity",LocalDate.now().plusMonths(1),false));
+		todos.add(new Todo(++todoCount,"Khunwana", "master FullStack dev",LocalDate.now().plusMonths(1),false));
+	}
+	
+	public List<Todo> findByUsername(String username)
+	{
+		Predicate<? super Todo> predicate = todo -> todo.getUsername().equalsIgnoreCase(username);
+		
+		return todos.stream().filter(predicate).toList();
+	}
+	
+	public void addnewTodo(String username, String description, LocalDate targetDate, boolean done)
+	{
+		todos.add(new Todo(++todoCount,username,description,targetDate,done));
+	}
+	
+	public void deleteById(int id)
+	{
+		Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+		todos.removeIf(predicate);
+		
+	}
+	
+
+	public Todo findById(int id) {
+		Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+		Todo todo = todos.stream().filter(predicate).findFirst().get();
+		return todo;
+	}
+
+	public void updateTodo(@Valid Todo todo) {
+		deleteById(todo.getId());
+		todos.add(todo);		
+	}
+	
+	
+}

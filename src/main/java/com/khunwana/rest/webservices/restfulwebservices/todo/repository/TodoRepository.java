09801fa3fb4dd01046372fbac206079d790e4400
Monias <khunwana.monias@gmail.com>
2024-06-14 18:15:01 +0200

    interface connectivity to ToDo class
    Signed-off-by: Monias <khunwana.monias@gmail.com>

---
+++ src/main/java/com/khunwana/rest/webservices/restfulwebservices/todo/repository/TodoRepository.java
@@ -0,0 +1,11 @@
+package com.khunwana.rest.webservices.restfulwebservices.todo.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.khunwana.rest.webservices.restfulwebservices.todo.Todo;
+
+public interface TodoRepository extends JpaRepository<Todo, Integer> {
+	List<Todo> findByUsername(String username);
+}
